7159a2912982580edc8e00ea5abb1231b5c38cc0
naokato <Naoya@okamotohitomi-no-MacBook-Air.local>
2023-03-31 23:22:19 +0900

    add local search code to a46

--- a46/Main.cpp
+++ a46/Main.cpp
@@ -1,15 +1,29 @@
 #define DEBUG 0
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int N, X[159], Y[159];
 int answer[159];
-bool visited[159];
+double score;
+
+int RandInt(int a, int b) {
+  return a + rand() % (b - a + 1);
+}
 
 double calcDistance(int x1, int y1, int x2, int y2) {
   return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
+
+double GetScore() {
+  double distance = 0;
+  for (int i = 1; i <= N; i++) {
+    distance += calcDistance(X[answer[i]], Y[answer[i]], X[answer[i + 1]], Y[answer[i + 1]]);
+  }
+  return distance;
+}
+
 
 int main() {
   // input
@@ -17,39 +31,32 @@ int main() {
   for (int i = 1; i <= N; i++) cin >> X[i] >> Y[i];
 
   // initialize
-  for (int i = 1; i <= N; i++) visited[i] = false;
-  answer[1] = 1;
-  visited[1] = true;
-  double totalDistance = 0;
+  for (int i = 1; i <= N; i++) answer[i] = i;
+  answer[N + 1] = 1;
 
-  for (int i = 2; i <= N; i++) {
-    int currentCity = answer[i - 1];
-    double minDistance = 10000000;
-    int minCity = 0;
+  score = GetScore();
+  for (int i = 1; i <= 200000; i++) {
+    int L = RandInt(2, N);
+    int R = RandInt(2, N);
+    if (L > R) swap(L, R);
 
-    for (int j = 1; j <= N; j++) {
-      if (visited[j]) continue;
-
-      double distance = calcDistance(X[currentCity], Y[currentCity], X[j], Y[j]);
-      if (minDistance > distance) {
-        minDistance = distance;
-        minCity = j;
-      }
+    reverse(answer + L, answer + R + 1);
+    double currentScore = GetScore();
+    if (score >= currentScore) {
+      score = currentScore;
+    } else {
+      // if the score is not good, undo reverse
+      reverse(answer + L, answer + R + 1);
     }
-
-    answer[i] = minCity;
-    visited[minCity] = true;
-    totalDistance += minDistance;
   }
-  totalDistance += calcDistance(X[answer[N]], Y[answer[N]], X[1], Y[1]);
 
   #if DEBUG
-  cout << totalDistance << endl;
+  cout << score << endl;
   #endif
 
-  for (int i = 1; i <= N; i++) {
+  for (int i = 1; i <= N + 1; i++) {
     cout << answer[i] << endl;
   }
-  cout << 1 << endl;
+
   return 0;
 }
